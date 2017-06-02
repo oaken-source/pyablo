aa4a9fe51a9f85183775014f93c832ca2e028fca
Andreas Grapentin <andreas@grapentin.org>
2017-06-02 11:21:52 +0200

    adding sound

--- pyablo/__main__.py
+++ pyablo/__main__.py
@@ -1,29 +1,46 @@
 import pygame
+from pygame.locals import QUIT, VIDEORESIZE
 import mpq
 import av
 
-resources = mpq.MPQFile('resources/diabdat.mpq')
+pygame.mixer.pre_init(channels=1)
+pygame.init()
 
-def cutscene(resource, repeat=1):
-    video = av.open(resources.open(resource))
+RESOURCES = mpq.MPQFile('resources/diabdat.mpq')
 
-    for frame in video.decode(video=0):
-        image = pygame.image.frombuffer(frame.to_rgb().to_nd_array(),
-            (frame.width, frame.height), 'RGB')
-        for _ in range(repeat):
+def cutscene(resource):
+    def video():
+        data = av.open(RESOURCES.open(resource))
+        for frame in data.decode(video=0):
+            image = pygame.image.frombuffer(
+                frame.to_nd_array(format='rgb24'),
+                (frame.width, frame.height), 'RGB')
             yield image
+
+    fifo = av.AudioFifo()
+    data = av.open(RESOURCES.open(resource))
+    for frame in data.decode(audio=0):
+        fifo.write(frame)
+
+    resampler = av.AudioResampler(format='s16p', layout='mono')
+    audio = pygame.mixer.Sound(array=resampler.resample(fifo.read()).to_nd_array()[0])
+
+    return video(), audio
 
 
 def scale(frame, screen):
-    scale = min(screen.get_width() / frame.get_width(),
+    factor = min(
+        screen.get_width() / frame.get_width(),
         screen.get_height() / frame.get_height())
 
-    return pygame.transform.smoothscale(frame,
-        (round(frame.get_width() * scale), round(frame.get_height() * scale)))
+    return pygame.transform.smoothscale(
+        frame,
+        (round(frame.get_width() * factor), round(frame.get_height() * factor)))
 
 
 def center(frame, screen):
-    return ((screen.get_width() - frame.get_width()) / 2,
+    return (
+        (screen.get_width() - frame.get_width()) / 2,
         (screen.get_height() - frame.get_height()) / 2)
 
 
@@ -31,18 +48,35 @@ def main():
     screen = pygame.display.set_mode((640, 480))
     clock = pygame.time.Clock()
 
-    for frame in cutscene('File00002910.smk', repeat=2):
+    frames, audio = cutscene('File00002910.smk')
+    audio.play()
+    for frame in frames:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == QUIT:
                 return
-            elif event.type == pygame.VIDEORESIZE:
+            elif event.type == VIDEORESIZE:
                 screen = pygame.display.set_mode(event.dict['size'])
 
         frame = scale(frame, screen)
         screen.blit(frame, center(frame, screen))
 
         pygame.display.flip()
-        clock.tick(30)
+        clock.tick(15)
+
+    frames, audio = cutscene('File00001475.smk')
+    audio.play()
+    for frame in frames:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return
+            elif event.type == VIDEORESIZE:
+                screen = pygame.display.set_mode(event.dict['size'])
+
+        frame = scale(frame, screen)
+        screen.blit(frame, center(frame, screen))
+
+        pygame.display.flip()
+        clock.tick(15)
 
 
 if __name__ == '__main__':
